459cdabc00b5cad8a1c05c5076b612ecfb346e82
Ivan Trajanovski <ivan.trajanovski@egroup.hu>
2021-07-28 09:26:18 +0200

    connect database

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-const express = require('express');
-const app = express()
-const path = require('path')
-const mongoose = require('mongoose')
-const Campground = require('./models/campground')
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
-mongoose.connect('mongodb://localhost:27022/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => {
-    console.log("Database Connected")
-})
+  console.log("Database Connected");
+});
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'Cheap camping' })
-    await camp.save()
-    res.send(camp)
-})
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "My Backyard",
+    description: "Cheap camping",
+  });
+  await camp.save();
+  res.send(camp);
+});
 
 app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
+  console.log("Serving on port 3000");
+});
